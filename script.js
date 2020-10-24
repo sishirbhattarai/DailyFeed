9239975241ea05e1dd840d6458b850b1cc14026c
GnomeSaiyan <69124994+jeffreytwoods2@users.noreply.github.com>
2020-10-24 12:10:31 -0600

    Made locaiton DOM update automatic

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var mapURL;
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showPosition)
     } else {
         locationEl.innerHTML = "Geolocation is not supported by this browser.";
     };
@@ -24,17 +24,12 @@ function getLocation() {
 function showPosition(position) {
     userLat = position.coords.latitude;
     userLong = position.coords.longitude;
-    mapURL = "http://www.mapquestapi.com/geocoding/v1/reverse?key=jHLf4uATR4fijVkLOmrimhIJE79Xp0kx&location=" + userLat + "," + userLong
-};
-getLocation();
-
-// This ajax must run AFTER user allows geolocation; that's why it's behind a button
-locationBtn.on('click', function () {
-
+    mapURL = "http://www.mapquestapi.com/geocoding/v1/reverse?key=jHLf4uATR4fijVkLOmrimhIJE79Xp0kx&location=" + userLat + "," + userLong;
     $.ajax({
         url: mapURL,
         method: "GET"
     }).then(function (response) {
         cityEl.text("You live in " + (response.results[0].locations[0].adminArea5) + ".");
     });
-})
+};
+getLocation();

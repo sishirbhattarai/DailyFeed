65edb62cb7e43efb5712c24b162f984f9c0f39f8
Jack <jckkempton@gmail.com>
2020-10-24 10:54:15 -0600

    removed all zomato elements

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 // Our JavaScript goes here
-// Zomato API Global Variables
-// var zomatoCityID = "";
-// var zomatoQuery = "";
-// var zomatoLat = "";
-// var zomatoLon = "";
-var zomSearch = $("#search-btn");
-var zomInput = "";
+
 // WeatherBit API
 var weatherbitURL = "https://api.weatherbit.io/v2.0/current?city=" + weatherbitCity + "key=aa00598f57b74bddb364a7b526faf997";
 var weatherbitCity = "";
@@ -17,24 +11,6 @@ var cityEl = $("#location");
 var userLat;
 var userLong;
 var mapURL;
-
-function zomatoCall() {
-    var zomatoURL = "https://developers.zomato.com/api/v2.1/cities?q=" + zomInput + "&apikey=54aedad9a5cf457cabacf6702d606833";
-    $.ajax({
-        url: zomatoURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        
-    });
-}
-
-$(zomSearch).on('click', function (event) {
-    event.preventDefault();
-    zomInput = $("#zomatoInput").val().trim();
-    
-    zomatoCall();
-});
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -55,9 +31,6 @@ getLocation();
 locationBtn.on('click', function () {
 
     $.ajax({
-        header: {
-            'user-key': 'jHLf4uATR4fijVkLOmrimhIJE79Xp0kx'
-        },
         url: mapURL,
         method: "GET"
     }).then(function (response) {

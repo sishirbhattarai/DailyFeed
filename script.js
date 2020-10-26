595fd537019585e4f2967e09816418edde85964d
GnomeSaiyan <69124994+jeffreytwoods2@users.noreply.github.com>
2020-10-25 21:39:19 -0600

    Side-by-side layout, news search functionality, icons

--- script.js
+++ script.js
@@ -3,27 +3,46 @@
 // WeatherBit API
 var weatherbitURL = "https://api.weatherbit.io/v2.0/current?city=" + weatherbitCity + "key=aa00598f57b74bddb364a7b526faf997";
 var weatherbitCity = "";
+var icon = $("<i>");
+var tempDiv = $("#temperature");
+var tempIcon = $("#temp-icon");
+var weather = $("#description");
+var weatherImg = $("<img>");
+var uvDiv = $("#uv");
+var uvIcon = $("<i class=\"fas fa-sun\">");
 // MapQuest API Global Variables
 var locationBtn = $("#location-btn");
 var cityEl = $("#location");
 var userLat;
 var userLong;
-var mapURL;
 // GuardianAPI Global Variables
-var guardianSearch = "";
+var guardianSearch;
 var guardianURL = "https://content.guardianapis.com/search?q=" + guardianSearch + "&api-key=199bdec0-409f-48d7-a79a-6ff10791c23e";
+var searchBtn = $("#search-btn");
+var articles = $("#articles");
 // Covid19API Global Variables
 
 
-function callGuardian () {
+searchBtn.on('click', function(event) {
+    event.preventDefault();
+    guardianSearch = $("#news-search").val();
+    articles.empty();
+    console.log(guardianSearch);
     $.ajax({
-        url: guardianURL,
+        url: "https://content.guardianapis.com/search?q=" + guardianSearch + "&api-key=199bdec0-409f-48d7-a79a-6ff10791c23e",
         method: "GET"
     }).then(function(response){
         console.log(response);
-    })
-}
-callGuardian();
+        console.log(response.response.results[0]);
+        for (i = 0; i < 4; i++) {
+            articles.append("<hr>");
+            var story = $("<a>");
+            story.attr("href", response.response.results[i].webUrl);
+            story.text(response.response.results[i].webTitle);
+            articles.append(story);
+        }
+    });
+});
 
 // Asks permission to get user's location data
 function getLocation() {
@@ -40,67 +59,43 @@ function getLocation() {
 function showPosition(position) {
     userLat = position.coords.latitude;
     userLong = position.coords.longitude;
-    mapURL = "http://www.mapquestapi.com/geocoding/v1/reverse?key=jHLf4uATR4fijVkLOmrimhIJE79Xp0kx&location=" + userLat + "," + userLong;
     $.ajax({
-        url: mapURL,
+        url: "http://www.mapquestapi.com/geocoding/v1/reverse?key=jHLf4uATR4fijVkLOmrimhIJE79Xp0kx&location=" + userLat + "," + userLong,
         method: "GET"
     }).then(function (response) {
-        cityEl.text("Your current location is " + (response.results[0].locations[0].adminArea5) + "," + (response.results[0].locations[0].adminArea3) + ".");
-        console.log(response)
+        cityEl.text((response.results[0].locations[0].adminArea5) + ", " + (response.results[0].locations[0].adminArea3));
+        console.log(response);
+        getWeather()
     });
 };
+
 getLocation();
 
-  navigator.geolocation.getCurrentPosition(success, error);
+// Passes user coords into weather api, returns local weather info
+function getWeather() {
 
-  function success(position) {
-      console.log(position)
-  }
-  function error() {}
-  getWeather();
-
-  function getWeather() {
-  
-    navigator.geolocation.getCurrentPosition(success, error);
-
-    function success(position) {
-      latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
-
-//Ajax call on weather
-  var queryURL = "https://api.weatherbit.io/v2.0/current" + "?lat=" + latitude + "&lon=" + longitude + "&key=b2da4d28d2ce4023b0d33cbef6a3f959";
-      $.ajax({
-        url: queryURL,
+    $.ajax({
+        url: "https://api.weatherbit.io/v2.0/current?lat=" + userLat + "&lon=" + userLong + "&key=b2da4d28d2ce4023b0d33cbef6a3f959",
         method: "GET"
-      })
-      .then(function(response) {
+    }).then(function(response) {
 
         console.log(response);
-
-        // Log the resulting object
-        console.log(response.data[0].temp);
+        console.log("Celsuis temperature: " + response.data[0].temp);
         var tempF = response.data[0].temp * 1.80 + 32;
-        var icon =  $("<img>").attr("src", "https://www.weatherbit.io/api/codes" + response.data[0].weather.icon + ".png")
-        //Console loging temperature on Farenheit
-        console.log(tempF);
-        $("#datetime").text(response.data[0].datetime);
-        $("#temperature").text("Temp: " + response.data[0].temp);
-        $("#temperature").text("Temp: " + tempF.toFixed(2) + "°F");
-        $("#description").text( response.data[0].weather.description);
-        $("#uv").text("UV Index: " + response.data[0].uv);
-        //$("#icon").text(imag);
-     
-        // var queryURL = "https://api.weatherbit.io/v2.0/current/airquality" + "?lat=" + latitude + "&lon=" + longitude + "&key=b2da4d28d2ce4023b0d33cbef6a3f959";
-        // $.ajax({
-        //   url: queryURL,
-        //   method: "GET"
-        // })
-        // .then(function(response) {
-  
-        //   console.log(response);
-  
-        //   // Log the resulting object
-        //  // console.log(response.data[0].temp);
-        // })
-      });
-    }}
+        console.log("Fahrenheit temperature: " + tempF);
+
+        if (tempF < 60) {
+            icon.addClass("fas fa-temperature-low");
+            tempIcon.append(icon)
+        } else {
+            icon.addClass("fas fa-temperature-high");
+            tempIcon.append(icon)
+        };
+        tempDiv.append(" " + tempF.toFixed(2) + "° F");
+        weatherImg.attr("src", "icons/" + response.data[0].weather.icon + ".png");
+        weather.append(weatherImg);
+        weather.append(response.data[0].weather.description);
+        uvDiv.append(uvIcon);
+        uvDiv.append(" UV Index: " + response.data[0].uv.toFixed(2));
+    });
+}
